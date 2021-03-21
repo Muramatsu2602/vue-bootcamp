daa287c2f10ef6f76bd965a06c390a24fa255662
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-03-20 23:53:27 -0300

    day3: cleaning code and creating Component

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,13 @@
 <template>
-  <!-- <div>
-    <h1>Lesson 2: Yoyo's</h1>
-    <options />
-    <composition-api />
-
-    <h1>Lesson 2: Challenge</h1>
-    <meu-composition />
-    <meu-option />
-
-    <h1>Lesson 3: Yoyo's</h1>
-    <div>Hello</div>
-    <div>Total: {{ total }}</div>
-
-    <options c="Componente options 1" :total="total" @inc="incHandler" />
-    <options c="Componente options 2" @inc="incHandler" />
-    <options c="Componente options 3" @inc="incHandler" />
-
-    <composition-api text="Componente Composition 1" @inc="incHandler" />
-    <composition-api text="Componente Composition 2" @inc="incHandler" />
-    <composition-api text="Componente Composition 3" @inc="incHandler" />
-  </div> -->
-
   <div id="nav">
-    <router-link to="/">Home </router-link>
-    <router-link to="/about">About </router-link>
+    <router-link to="/">Home </router-link> |
+    <router-link to="/about">About </router-link> |
     <router-link to="/components">Components </router-link>
   </div>
+
+  <!-- CREATE AN HEADER BEFORE ROUTER VIEW TO PRESERVE ITS LAYOUT WHEN TRANSITIONING THROUGH PAGES -->
   <router-view />
 </template>
-
-<script>
-import { ref } from "vue";
-import Options from "@/components/OptionsApi.vue";
-import CompositionApi from "@/components/CompositionApi.vue";
-
-import MeuOption from "@/components/desafio-aula2/MeuOption.vue";
-import MeuComposition from "@/components/desafio-aula2/MeuComposition.vue";
-
-export default {
-  components: {
-    Options,
-    CompositionApi,
-    MeuComposition,
-    MeuOption,
-  },
-
-  setup() {
-    const total = ref(0);
-
-    const incHandler = () => {
-      console.log("total", total.value);
-      total.value += 1;
-    };
-
-    return { total, incHandler };
-  },
-};
-</script>
 
 <style lang="scss">
 #app {
@@ -73,6 +24,9 @@ export default {
   a {
     font-weight: bold;
     color: #2c3e50;
+
+    margin: 0.5rem;
+    text-decoration: none;
 
     &.router-link-exact-active {
       color: #42b983;
--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>Sou o View componente</div>
+  <div>
+    <h1>Lesson 2: Yoyo's</h1>
+    <options />
+    <composition-api />
+
+    <h1>Lesson 2: Challenge</h1>
+    <meu-composition />
+    <meu-option />
+
+    <h1>Lesson 3: Yoyo's</h1>
+    <div>Hello</div>
+    <div>Total: {{ total }}</div>
+
+    <options c="Componente options 1" :total="total" @inc="incHandler" />
+    <options c="Componente options 2" @inc="incHandler" />
+    <options c="Componente options 3" @inc="incHandler" />
+
+    <composition-api text="Componente Composition 1" @inc="incHandler" />
+    <composition-api text="Componente Composition 2" @inc="incHandler" />
+    <composition-api text="Componente Composition 3" @inc="incHandler" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { ref, defineComponent } from "vue";
+import Options from "@/components/OptionsApi.vue";
+import CompositionApi from "@/components/CompositionApi.vue";
 
-export default defineComponent({});
+import MeuOption from "@/components/desafio-aula2/MeuOption.vue";
+import MeuComposition from "@/components/desafio-aula2/MeuComposition.vue";
+
+export default defineComponent({
+  components: {
+    Options,
+    CompositionApi,
+    MeuComposition,
+    MeuOption,
+  },
+
+  setup() {
+    const total = ref(0);
+
+    const incHandler = () => {
+      console.log("total", total.value);
+      total.value += 1;
+    };
+
+    return { total, incHandler };
+  },
+});
 </script>
