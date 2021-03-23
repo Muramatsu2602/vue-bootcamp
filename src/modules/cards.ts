5659c3a7d08841dd7fc2547e503c9e50dec36253
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-03-23 00:38:52 -0300

    Day5: card interface

--- src/modules/cards.ts
+++ src/modules/cards.ts
@@ -1,9 +1,21 @@
 import { readonly } from "vue";
 
+// ---------------------- Interface ------------------
+interface Card {
+  id: number;
+  name: string;
+  price: number;
+  attack: number;
+  defence: number;
+}
+
 interface State {
   list: [];
 }
 
+// -------------------------------------------
+
+// the cards will go here!
 const state: State = {
   list: []
 };
